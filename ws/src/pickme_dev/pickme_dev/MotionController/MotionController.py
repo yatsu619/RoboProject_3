b14009b07f64641cdc0b854202d522006e3aeba0
Marcel K <modgamerlp@gmail.com>
2026-05-04 01:29:07 +0200

    Roboter sollte sich jetzt im initialen movement homen indem er die positiven anschläge anfährt.

--- ws/src/pickme_dev/pickme_dev/MotionController/MotionController.py
+++ ws/src/pickme_dev/pickme_dev/MotionController/MotionController.py
@@ -10,7 +10,13 @@ from rclpy.duration import Duration
 
 from std_msgs.msg import Bool
 
-TIMEBASE = 0.1    # Timebase for the timer and PrimThread [s]
+# Timer Timebase in [s]
+TIMEBASE = 0.1
+
+# Toolcenterpoint offset in [m]
+TCP_OFFSET_X = 0
+TCP_OFFSET_Y = 0
+TCP_OFFSET_Z = 0
 
 class MotionControllerNode(Node):
     def __init__(self):
@@ -45,32 +51,38 @@ class MotionControllerNode(Node):
             10
         )
 
+        # Timed loop for Primary code
         self.timer = self.create_timer(TIMEBASE, self.PrimThread, callback_group=ReentrantCallbackGroup())
         
+        # Message instance
         self.cmd = RobotCmd()
-        
-        self.coords_ready = False
 
+        # Current robot position transmitted by robot
         self.robot_x = 0
         self.robot_y = 0
         self.robot_z = 0
         self.robot_pos_lock = threading.Lock()
 
+        # Current homing position
+        # when homing is complete the then current position becomes an offset added to homepos
+        self.homepos_x = 0
+        self.homepos_y = 0
+        self.homepos_z = 0
+
+        # Point received via topic
         self.dist_x = 0
         self.dist_y = 0
         self.dist_z = 0
         self.external_debug_lock = threading.Lock()
 
-        self.axis_speed = [0, 0, 0]
-        self.axis_time = [0, 0, 0]
-
-        self.last_distance = 0
-        self.distance_left = [0, 0, 0]
-
+        # PD-Controller instances
         self.controller_logic = MotionControllerLogic()
         self.controller_x = Controller()
         self.controller_y = Controller()
         self.controller_z = Controller()
+
+        # Self explanatory
+        self.init_complete = False
     
 
     def external_debug_callback(self, msg: ExtDebug):
@@ -101,9 +113,48 @@ class MotionControllerNode(Node):
     def PrimThread(self):
         """Primary thread that executes all the code"""
 
-        self.cmd.accel_x = self.controller_x.PDController(self.dist_x, self.robot_x, 1, 2, TIMEBASE)
-        self.cmd.accel_y = self.controller_y.PDController(self.dist_y, self.robot_y, 1, 2, TIMEBASE)
-        self.cmd.accel_z = self.controller_z.PDController(self.dist_z, self.robot_z, 1, 2, TIMEBASE)
+        if self.init_complete == False:
+            self.AccelerateAxis("X", 0)
+            self.AccelerateAxis("Y", 0)
+            self.AccelerateAxis("Z", 0)
+            self.DriveToHomePos()
+
+            # Change to dynamically calculated sleep
+            time.sleep(20)
+            self.homepos_x = self.robot_x - TCP_OFFSET_X
+            self.homepos_y = self.robot_y - TCP_OFFSET_Y
+            self.homepos_z = self.robot_z - TCP_OFFSET_Z
+        else:
+            self.cmd.accel_x = self.controller_x.PDController(self.dist_x, self.robot_x, 1, 2, TIMEBASE)
+            self.cmd.accel_y = self.controller_y.PDController(self.dist_y, self.robot_y, 1, 2, TIMEBASE)
+            self.cmd.accel_z = self.controller_z.PDController(self.dist_z, self.robot_z, 1, 2, TIMEBASE)
+            self.publisher_command.publish(self.cmd)
+
+    
+    def DriveToHomePos(self):
+        """Logic that drives all axis to their homing position
+        (Endstops)"""
+
+        self.AccelerateAxis("X", self.controller_logic.accel_avg)
+        self.AccelerateAxis("Y", self.controller_logic.accel_avg)
+        self.AccelerateAxis("Z", self.controller_logic.accel_avg)
+        time.sleep(1)
+        self.AccelerateAxis("X", 0)
+        self.AccelerateAxis("Y", 0)
+        self.AccelerateAxis("Z", 0)
+
+    def AccelerateAxis(self, axis: str, accel: int) -> None:
+        """Wrapper to accelerate axis for 1 second. Used in
+        "DriveToHomePos" only and to initialize acceleration
+        to 0 in the beginning."""
+
+        if axis == "X":
+            self.cmd.accel_x = accel
+        elif axis == "Y":
+            self.cmd.accel_y = accel
+        elif axis == "Z":
+            self.cmd.accel_z = accel
+        
         self.publisher_command.publish(self.cmd)
 
 

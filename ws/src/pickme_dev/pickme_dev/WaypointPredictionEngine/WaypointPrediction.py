192b93fc523bbab0d767e3d986ee07e7a18bae1c
johannes-wk <wk8228763@gmail.com>
2026-06-10 11:13:48 +0200

    Refactor WaypointPredictionNode for improved message handling and prediction accuracy; streamline buffer management and calculations

--- ws/src/pickme_dev/pickme_dev/WaypointPredictionEngine/WaypointPrediction.py
+++ ws/src/pickme_dev/pickme_dev/WaypointPredictionEngine/WaypointPrediction.py
@@ -32,61 +32,64 @@ class WaypointPreditionNode(Node):
         self.timer = self.create_timer(0.2, self.timer_callback)
         self.x_buffer = deque(maxlen=5)
         self.y_buffer = deque(maxlen=5)
-       
-        
+        self.time_buffer= deque(maxlen=5)
+        self.Föderband_layer = 0.0 
         self.last_msg= None
-        self.last_msg_avg = None
-        self.lookahead_sec = None  # die zeit wo wir die kordinaten von dem objekt in der zukunft berechnen      
-        self.z = 0.0  # das förderband ist laut kordinaten ursprung layer null in der z achse 
+        self.obj_id=None
+        
+        
         self.get_logger().info('WaypointPredictionNode gestartet.')
 
     def robot_pos_callback(self, msg: CamData):
-        if self.last_msg_avg is None:
-            self.last_msg_avg = self.moving_average(msg.x,msg.y)
-            self.last_msg=msg
-            return
-    def timer_callback(self):
-        dt = self.time_diff_sec(self.last_msg.timestamp, msg.timestamp)
-        if dt <= 0.0:
-            self.get_logger().warn('Ungültiger Zeitunterschied.')
-            self.last_msg = msg
-            return
-        avg_x, avg_y = self.moving_average(msg.x, msg.y)
-        vx = (avg_x - self.last_msg_avg[0]) / dt
-        vy = (avg_y - self.last_msg_avg[1]) / dt
+       
+        self.x_buffer.append(msg.x)
+        self.y_buffer.append(msg.y)
+        self.time_buffer.append(msg.timestamp)
+        self.obj_id=msg.obj_type 
 
-        
-
-        
     
+    def timer_callback(self):
+        if len(self.x_buffer) > 0 and self.last_msg==None :
+            self.last_msg=self.moving_average
+            self.x_buffer=None
+            self.y_buffer=None
+            self.time_buffer=None
+
+        if self.last_msg!= None :
+            self.aktuelle_Werte_msg =self.moving_average
+            dt=self.time_diff_sec(self.aktuelle_Werte_msg[3],self.last_msg[3])
+            vx=self.berechnung_Geschwindigkeit(self.aktuelle_Werte_msg[1],self.last_msg[1],dt )
+        else :
+            return
+
 
         pred_msg = PredictedPosdelay()
         
         pred_msg.vx = vx
-        pred_msg.y = msg.y
-        pred_msg.z = self.z 
-        pred_msg.obj_zero = obj_zero
-        pred_msg.obj_id = msg.obj_type 
+        pred_msg.y = self.aktuelle_Werte_msg[2]
+        pred_msg.z = self.Föderband_layer 
+        pred_msg.x= self.aktuelle_Werte_msg[1]
+        pred_msg.obj_id = self.obj_id
 
         self.publisher_prediction.publish(pred_msg)
 
         
 
-        self.last_msg = msg
-        self.last_msg_avg = (avg_x, avg_y)
+        self.last_msg =None
+    def berechnung_Geschwindigkeit(self,avg_x,last_avg_x,dt):
+        vx=(avg_x-last_avg_x)/dt
+        return vx 
 
     def time_diff_sec(self, t1, t2) -> float:
-        ''' funktion die den zeitunterschied berechnet  '''
-        return (t2 - t1)
+        
+        return abs(t2 - t1)
     
-    def moving_average(self, x, y):
-        self.x_buffer.append(x)
-        self.y_buffer.append(y)
-
+    def moving_average(self):
+       
         avg_x = sum(self.x_buffer) / len(self.x_buffer)
         avg_y = sum(self.y_buffer) / len(self.y_buffer)
-
-        return avg_x, avg_y
+        avg_time=sum(self.time_buffer) / len(self.time_buffer)
+        return avg_x, avg_y, avg_time
 
 def main(args=None):
     rclpy.init(args=args)

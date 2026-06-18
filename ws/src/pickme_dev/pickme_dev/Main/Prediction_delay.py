c8116fb6c998af92d58c78f0c07b9ddad7f7a560
johannes-wk <wk8228763@gmail.com>
2026-06-18 15:32:10 +0200

    Refactor DelayBufferNode to improve object handling and state management

--- ws/src/pickme_dev/pickme_dev/Main/Prediction_delay.py
+++ ws/src/pickme_dev/pickme_dev/Main/Prediction_delay.py
@@ -17,8 +17,9 @@ class DelayBufferNode(Node):
 
         
         self.obj_buffer = deque()
+        self.last_x_per_objtype={}
         self.active_obj= None
-        self.obj_geholt=False
+        self.is_gripping=False 
         self.activ_gripper=False
         self.last_x=None
         self.min_abstand_obj= 0.025
@@ -69,58 +70,56 @@ class DelayBufferNode(Node):
             "obj_id": msg.obj_id,
             "zeitpunkt_logging": time.time(), 
         }
+        obj_id=obj["obj_id"]
+        x=obj["x"]
+        last_x=self.last_x_perobjtype.get(obj_id,None)
 
-       
-        if self.last_x is None or abs(obj["x"] - self.last_x) >= self.min_abstand_obj :
+        if self.last_x is None or abs(x - last_x) >= self.min_abstand_obj :
             self.obj_buffer.append(obj) #Objekt erstes mal oder neues objekt  ->  Objekt puffern
             self.get_logger().info(
                 f"Neues Objekt gepuffert (obj_id={obj['obj_id']}), "
                 f"Pufferlänge={len(self.obj_buffer)}"
                 )
-        self.last_x = obj["x"]
+        self.last_x_per_objtype[obj_id]=x
     
 
     def gripper_callback(self,msg:RobotCmd) :
-        self.activ_gripper=msg.activate_gripper
-
-
+        new_gripper_state=msg.activate_gripper
+        if new_gripper_state and not self.is_gripping:
+                self.is_gripping = True
+                self.activ_gripper = True
+        if not new_gripper_state:
+            self.is_gripping = False
+            self.activ_gripper = False
+            self.active_obj = None
 
     def timer_callback(self):
        
-        if self.activ_gripper == True and self.obj_geholt == False :
+        if self.is_gripping == True and not self.is_gripping:
             if len(self.obj_buffer) > 0:
                 self.active_obj = self.obj_buffer.popleft()
-                self.obj_geholt= True
+                self.is_gripping= True
                 self.get_logger().info(
                     f"Nach Greifprozess: neues Objekt aus Puffer aktiviert "
                     f"(obj_id={self.active_obj['obj_id']})"
                 )
+            return
+            if not self.activ_gripper:
                 return
-            
-            elif len(self.obj_buffer) <=0:
+            if self.active_obj is None:
+                if len(self.obj_buffer)>0 :
+                    self.active_obj = self.obj_buffer.popleft()
+                    self.is_gripping = True    
+                else:
+                    return
+            if  len(self.obj_buffer) <=0:
                 self.active_obj= None
                 self.get_logger().info(
                     f"Nach Greifprozess: kein objekt im puffer "
                 )
             return
         
-        if self.activ_gripper== False and self.obj_geholt== True:
-            self.obj_geholt= False
-
-
-
-        if self.active_obj== None and len(self.obj_buffer)>0: 
-            self.active_obj = self.obj_buffer.popleft()
-            self.obj_geholt= True
-            self.get_logger().info(
-                    f"inizial : erstes Objekt aus Puffer aktiviert "
-            )
-        elif self.active_obj== None and len(self.obj_buffer)<=0:
-            self.obj_geholt=False
-            self.get_logger().info(
-                    f"leer: kein objet vorhanden  "
-            )
-            return
+        
         
         x_zum_Startzeitpunkt= self.active_obj["x"]
         vx = self.active_obj["vx"]
@@ -134,7 +133,7 @@ class DelayBufferNode(Node):
        
 
         # Innerhalb des Greifprozesses -> Position berechnen und publizieren
-        if self.obj_geholt ==True :
+        if self.is_gripping
             dt = abs(time_now-time_logged)
             greifpunkt_x = vx * dt + x_zum_Startzeitpunkt
         
